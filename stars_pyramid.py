0093745b697e2cc47a83fc25d21fb61015f277f8
Ravi Joshi <alaricjoshi8@gmail.com>
2022-08-04 10:02:07 +0530

    first commit

---
+++ stars_pyramid.py
@@ -0,0 +1,12 @@
+n=int(input("Enter the number of rows: "))
+z=n
+for i in range(n):                            #loop for running number of row times
+    
+    for j in range(z):                        #loop for printing empty spaces in each row
+        print(" ", end="")
+    
+
+    for l in range(2*i+1):                    #loop for printing stars in each row
+        print("*", end="")   
+    print("\n")
+    z=z-1
